21e34ca768ebd4ac82ece26197fa4a24987d9a58
Sergei Egorov <segorov@pivotal.io>
2019-01-18 18:14:07 +0100

    fix compilation errors

--- src/test/java/reactor/kafka/AbstractKafkaTest.java
+++ src/test/java/reactor/kafka/AbstractKafkaTest.java
@@ -25,6 +25,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
