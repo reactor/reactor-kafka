11e63cfa149384b28a041b741de3dddf079ad975
Simon Baslé <sbasle@vmware.com>
2021-07-02 17:31:37 +0200

    [chores] Copyright switch Pivotal to VMware, polish headers (#223)
     - copyright header to VMware instead of Pivotal
     - in copyright headers, have date range end on 2021
     - review the start year of files, some had start year inconsistent
       with their history or were missing the notice altogether
     - polish copyright header: url indentation, blank line after license
    
    See reactor/reactor#682.

--- src/main/java/reactor/kafka/sender/package-info.java
+++ src/main/java/reactor/kafka/sender/package-info.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2016-2018 Pivotal Software Inc, All Rights Reserved.
+ * Copyright (c) 2016-2021 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       https://www.apache.org/licenses/LICENSE-2.0
+ *   https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,4 +17,5 @@
 /**
  * Reactor Kafka Sender API
  */
+
 package reactor.kafka.sender;
