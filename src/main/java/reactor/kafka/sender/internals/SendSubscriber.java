dfa65c34b4c8a6212c1de619487bcec57b708a2c
Lukas <115710922+kotlinlukas@users.noreply.github.com>
2023-02-08 17:59:31 +0100

    Fix invasive logging (#309) (#322)
    * Fix invasive logging
    
    * Update SendSubscriber.java
    
    * Update src/main/java/reactor/kafka/sender/internals/SendSubscriber.java
    
    Co-authored-by: Gary Russell <grussell@vmware.com>
    
    * Update SendSubscriber.java
    
    * Update SendSubscriber.java
    
    ---------
    
    Co-authored-by: Gary Russell <grussell@vmware.com>

--- src/main/java/reactor/kafka/sender/internals/SendSubscriber.java
+++ src/main/java/reactor/kafka/sender/internals/SendSubscriber.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 VMware Inc. or its affiliates, All Rights Reserved.
+ * Copyright (c) 2020-2023 VMware Inc. or its affiliates, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -97,7 +97,7 @@ class SendSubscriber<K, V, C> implements CoreSubscriber<ProducerRecord<K, V>> {
             }
 
             if (exception != null) {
-                DefaultKafkaSender.log.error("Sender failed", exception);
+                DefaultKafkaSender.log.trace("Sender failed: ", exception);
                 firstException.compareAndSet(null, exception);
                 if (senderOptions.stopOnError() || senderOptions.fatalException(exception)) {
                     onError(exception);
